b17a0c1424a30483ef2ec9d41336a2d15d72e200
Karchevskiy <geisterkirche@gmail.com>
2019-09-21 20:41:57 +0300

    filter improvement

--- src/main/java/deck/dto/DeckFilter.java
+++ src/main/java/deck/dto/DeckFilter.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import deck.model.Deck;
 import deck.model.Deck_;
 import deck.model.User;
-import deck.model.User_;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.lang.Nullable;
 
@@ -12,10 +11,7 @@ import org.springframework.lang.Nullable;
 public class DeckFilter {
 
     @Nullable
-    private String name;
-
-    @Nullable
-    private String description;
+    private String search;
 
 
     public DeckFilter() {
@@ -27,34 +23,21 @@ public class DeckFilter {
             return cb.or(cb.equal(root.get(Deck_.visibleAsPublic), true), cb.equal(root.get(Deck_.owner), user));
         };
 
-        if (name != null) {
-            Specification<Deck> cache = (root, query, cb) -> cb.like(root.get(Deck_.name), name);
-            spec.and(cache);
-        }
-
-        if (description != null) {
-            Specification<Deck> specFN = (root, query, cb) -> cb.like(root.get(Deck_.description), description);
-            spec.and(specFN);
+        if (search != null) {
+            Specification<Deck> cache = (root, query, cb) -> cb.like(root.get(Deck_.name), search);
+            Specification<Deck> specFN = (root, query, cb) -> cb.like(root.get(Deck_.description), search);
+            spec.and(cache.or(specFN));
         }
 
         return spec;
     }
 
     @Nullable
-    public String getName() {
-        return name;
+    public String getSearch() {
+        return search;
     }
 
-    public void setName(@Nullable String name) {
-        this.name = name;
-    }
-
-    @Nullable
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(@Nullable String description) {
-        this.description = description;
+    public void setSearch(@Nullable String search) {
+        this.search = search;
     }
 }
